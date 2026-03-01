e9920112a27cb92ab1ca85d174f15f947d0f1289
Jakub Magiera <jakubmagiera.it@gmail.com>
2026-03-01 12:26:09 +0100

    create main key dat

--- setup.py
+++ setup.py
@@ -13,16 +13,26 @@ from shuffling import shuffle_answers
 
 
 def mix_tests(input_params):
-    docx_dir = Path(input_params.output_dir) / "Testy"
-    dat_dir = Path(input_params.output_dir) / "KluczeDAT"
-    txt_dir = Path(input_params.output_dir) / "KluczeTXT"
-    images_dir = Path(input_params.output_dir) / "Images"
-    docx_dir.mkdir()
-    dat_dir.mkdir()
-    txt_dir.mkdir()
-    images_dir.mkdir()
+    dirs = {
+        "docx_dir": Path(input_params.output_dir) / "Testy",
+        "main_dat_dir": Path(input_params.output_dir) / "KluczG",
+        "dat_dir": Path(input_params.output_dir) / "KluczeDAT",
+        "txt_dir": Path(input_params.output_dir) / "KluczeTXT",
+        "images_dir": Path(input_params.output_dir) / "Images",
+    }
 
-    questions = parse_exam_file(input_params.file_path, images_dir)
+    for dir in dirs.values():
+        dir.mkdir()
+
+    questions = parse_exam_file(input_params.file_path, dirs["images_dir"])
+
+    main_dat_data = []
+    for q in questions:
+        main_dat_data.append(
+            f"{q.id} {" ".join("1" if a["is_correct"] else "0" for a in q.answers)} {q.weight}"
+        )
+    save_data_file("klucz_glowny", "dat", main_dat_data, "", dirs["main_dat_dir"])
+
     number_of_questions = len(questions)
 
     for i in range(1, input_params.copy_number + 1):
@@ -43,8 +53,8 @@ def mix_tests(input_params):
                 f"{q_num + 1} {" ".join(str(x + 1) if not x else '0' for x in order)} {q.weight}"
             )
 
-        save_document(docx, i, docx_dir)
-        save_data_file("k", "dat", dat_data, i, dat_dir)
-        save_data_file("klucz", "txt", txt_data, i, txt_dir)
+        save_document(docx, i, dirs["docx_dir"])
+        save_data_file("k", "dat", dat_data, i, dirs["dat_dir"])
+        save_data_file("klucz", "txt", txt_data, i, dirs["txt_dir"])
 
-    rmtree(images_dir)
+    rmtree(dirs["images_dir"])
